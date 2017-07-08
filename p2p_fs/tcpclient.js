0c91c1f36ae62a20219739644f4c0bce0512d690
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-07-08 09:39:33 +0200

    discovery-channel

--- p2p_fs/tcpclient.js
+++ p2p_fs/tcpclient.js
@@ -1,11 +1,18 @@
 /* tcpclient.js */
 
+const channel = require('discovery-channel')({dht: false})
+const channelId = process.argv[2]
 const net = require('net')
 const pump = require('pump')
 const fs = require('fs')
 
-const socket = net.connect(10419, 'localhost')
+channel.join(channelId)
 
-pump(socket, fs.createWriteStream(`file-${Date.now()}`), err => {
-  if (err) throw err
+channel.once('peer', (id, peer) => {
+  const socket = net.connect(peer.port, peer.host)
+  console.log(`[connected to a peer: ${JSON.stringify(peer)}]`)
+  console.log('[streaming inbound file to disk]')
+  pump(socket, fs.createWriteStream(`file-${Date.now()}`), err => {
+    if (err) throw err
+  })
 })
--- p2p_fs/tcpserver.js
+++ p2p_fs/tcpserver.js
@@ -1,11 +1,18 @@
 /* tcpserver.js */
 
+const channel = require('discovery-channel')({dht: false})
+const channelId = process.argv[2]
+const port = Number(process.argv[3])
 const net = require('net')
 const fs = require('fs')
 const pump = require('pump')
 
+channel.join(channelId, port)
+
 net.createServer(socket => {
+  console.log(`[a peer connected: ${JSON.stringify(socket.address())}]`)
+  console.log(`[serving file ${__filename}]`)
   pump(fs.createReadStream(__filename), socket, err => {
     if (err) throw err
   })
-}).listen(10419, 'localhost', () => console.log('l.o.p. 10419'))
+}).listen(port, '0.0.0.0', () => console.log(`l.o.p. ${port}`))
