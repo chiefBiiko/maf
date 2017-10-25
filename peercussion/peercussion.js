2559aeeedd37f6ab2948121e1df755b800187343
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-10-25 18:16:24 +0200

    cleaned up

--- peercussion/peercussion.js
+++ peercussion/peercussion.js
@@ -3,25 +3,20 @@ const hashToPort = require('hash-to-port')
 const levelup = require('level')
 const scuttleup = require('scuttleup')
 
-const panic = msg => {
-  console.error(msg)
+const me = process.argv[2]
+const channel = process.argv[3]
+if (!me || !channel || process.argv.slice(2).some(a => /-?h(elp)?$/i.test(a))) {
+  console.error('p2p chat\nusage: node peercussion me channel [dht] [dns]\n' +
+    '  me\t\tyour displayed name\n  channel\tthe channel to join\n' +
+    '  dht\t\tuse the bittorent DHT for discovery? default: true\n' +
+    '  dns\t\tuse (local) multicast DNS discovery? default: true')
   process.exit(1)
 }
 
-const me = process.argv[2]
-const channel = process.argv[3]
-const help = 'usage: node peercussion me channel dht dns\n' +
-  '  me\t\tyour displayed name\n  channel\tthe channel to join\n' +
-  '  dht\t\tuse the bittorent DHT for discovery? default: true\n' +
-  '  dns\t\t\tuse (local) multicast DNS discovery? default: true'
-
-if (!me || !channel || process.argv.slice(2).some(a => /-?h(elp)?$/i.test(a)))
-  panic(help)
-
 const logs = scuttleup(levelup(`${me}.db`), { valueEncoding: 'json' })
 process.stdin.on('data', data => {
-  if (!/^\s*$/.test(data.toString()))
-    logs.append({ username: me, message: data.toString().trim() })
+  const msg = data.toString().trim()
+  if (msg) logs.append({ username: me, message: msg })
 })
 logs.createReadStream({ live: true }).on('data', data => {
   console.log(`${data.entry.username}> ${data.entry.message}`)
