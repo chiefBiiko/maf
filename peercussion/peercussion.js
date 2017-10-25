938e5cb92fddda8de867d5de0b61093d788531d4
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-10-25 16:32:53 +0200

    reviewed

--- peercussion/peercussion.js
+++ peercussion/peercussion.js
@@ -1,35 +1,40 @@
-/* chat.js */
-
 const discoverySwarm = require('discovery-swarm')
 const hashToPort = require('hash-to-port')
-
 const levelup = require('level')
 const scuttleup = require('scuttleup')
 
+const panic = msg => {
+  console.error(msg)
+  process.exit(1)
+}
+
 const me = process.argv[2]
 const channel = process.argv[3]
+const help = 'usage: node peercussion me channel dht dns\n' +
+  '  me\t\tyour displayed name\n  channel\tthe channel to join\n' +
+  '  dht\t\tuse the bittorent DHT for discovery? default: true\n' +
+  '  dns\t\t\tuse (local) multicast DNS discovery? default: true'
 
-if (!me || !channel || /h(elp)?$/i.test(me)) {
-  return console.error('usage: node peercussion.js yourName channel\n' +
-                       '  yourName\tyour displayed name\n' +
-                       '  channel\tthe channel to join')
-} 
+if (!me || !channel || process.argv.slice(2).some(a => /-?h(elp)?$/i.test(a)))
+  panic(help)
 
 const logs = scuttleup(levelup(`${me}.db`), { valueEncoding: 'json' })
-
 process.stdin.on('data', data => {
-  logs.append({ username: me, message: data.toString().trim() })
+  if (!/^\s*$/.test(data.toString()))
+    logs.append({ username: me, message: data.toString().trim() })
 })
-
 logs.createReadStream({ live: true }).on('data', data => {
   console.log(`${data.entry.username}> ${data.entry.message}`)
 })
 
 const swarm = discoverySwarm({ 
-  stream: logs.createReplicationStream({ live: true }) 
+  dht: !/false/i.test(process.argv[4]), 
+  dns: !/false/i.test(process.argv[5])
 })
-
-swarm.on('connection', (con, info) => console.log('[ new peer connection ]'))
-
+swarm.on('connection', (socket, peer) => {
+  console.log(`[ new peer connection from ${peer.host}:${peer.port} ]`)
+  socket.pipe(logs.createReplicationStream({ live: true })).pipe(socket)
+})
 swarm.listen(hashToPort(me))
-swarm.join(channel)
+swarm.join(channel, { announce: true })
+
